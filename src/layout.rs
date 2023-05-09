b87869027c99799d880d1235ef676c30e9f10aac
dz1230 <dz123079@gmail.com>
2023-05-09 10:12:16 +0200

    added util enum

--- src/layout.rs
+++ src/layout.rs
@@ -1,11 +1,74 @@
 
 // these arent in an enum because rust doesnt allow const generics with enums yet (on stable releases)
 
+use std::str::FromStr;
+
 pub const X: usize = 0;
 pub const Y: usize = 1;
 pub const WIDTH: usize = 2;
 pub const HEIGHT: usize = 3;
 pub const FONT_SIZE: usize = 4;
+
+pub struct LayoutErr;
+
+pub enum LayoutValue {
+    X,
+    Y,
+    Width,
+    Height,
+    FontSize,
+    PaddingTop,
+    PaddingBottom,
+    PaddingLeft,
+    PaddingRight,
+    BorderTop,
+    BorderBottom,
+    BorderLeft,
+    BorderRight,
+    MarginTop,
+    MarginBottom,
+    MarginLeft,
+    MarginRight,
+    ContentWidth,
+    ContentHeight,
+    MaskedX,
+    MaskedY,
+    MaskedWidth,
+    MaskedHeight,
+}
+
+impl FromStr for LayoutValue {
+    type Err = LayoutErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "x" => Ok(LayoutValue::X),
+            "y" => Ok(LayoutValue::Y),
+            "width" => Ok(LayoutValue::Width),
+            "height" => Ok(LayoutValue::Height),
+            "font-size" => Ok(LayoutValue::FontSize),
+            "padding-top" => Ok(LayoutValue::PaddingTop),
+            "padding-bottom" => Ok(LayoutValue::PaddingBottom),
+            "padding-left" => Ok(LayoutValue::PaddingLeft),
+            "padding-right" => Ok(LayoutValue::PaddingRight),
+            "border-top" => Ok(LayoutValue::BorderTop),
+            "border-bottom" => Ok(LayoutValue::BorderBottom),
+            "border-left" => Ok(LayoutValue::BorderLeft),
+            "border-right" => Ok(LayoutValue::BorderRight),
+            "margin-top" => Ok(LayoutValue::MarginTop),
+            "margin-bottom" => Ok(LayoutValue::MarginBottom),
+            "margin-left" => Ok(LayoutValue::MarginLeft),
+            "margin-right" => Ok(LayoutValue::MarginRight),
+            "content-width" => Ok(LayoutValue::ContentWidth),
+            "content-height" => Ok(LayoutValue::ContentHeight),
+            "masked-x" => Ok(LayoutValue::MaskedX),
+            "masked-y" => Ok(LayoutValue::MaskedY),
+            "masked-width" => Ok(LayoutValue::MaskedWidth),
+            "masked-height" => Ok(LayoutValue::MaskedHeight),
+            _ => Err(LayoutErr),
+        }
+    }
+}
 
 pub struct  NodeLayoutInfo {
     values: Vec<Option<i32>>,
